5c4b03652fdc540f2dc1fe4d3dbf046a5038a79f
ElectroBoy404NotFound <89782177+ElectroBoy404NotFound@users.noreply.github.com>
2022-01-20 21:41:47 +0530

    Update OLEDDisplay.java

--- src/main/java/de/pi3g/pi/oled/OLEDDisplay.java
+++ src/main/java/de/pi3g/pi/oled/OLEDDisplay.java
@@ -217,7 +217,7 @@ public class OLEDDisplay {
         device = bus.getDevice(displayAddress);
         this.rotation = rotation;
 
-        LOGGER.log(Level.INFO, "Opened i2c bus");
+        LOGGER.log(Level.DEBUG, "Opened i2c bus");
 
         clear();
 
@@ -422,7 +422,7 @@ public class OLEDDisplay {
             //now we close the bus
             bus.close();
         } catch (IOException ex) {
-            LOGGER.log(Level.INFO, "Closing i2c bus");
+            LOGGER.log(Level.DEBUG, "Closing i2c bus");
         }
     }
 
